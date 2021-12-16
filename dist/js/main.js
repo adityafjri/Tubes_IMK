983c793ba4884250ac081df72b432a2eaf4579b5
adityafjri <adityafjri@gmail.com>
2021-12-17 05:53:24 +0700

    edit index.html, add catalogue.html, add main.js

---
+++ dist/js/main.js
@@ -0,0 +1,46 @@
+const heroBtn = document.querySelector('.hero-btn');
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const logoJohn = document.querySelector('.logo')
+// const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
+
+let showMenu = false;
+
+heroBtn.addEventListener('click',redirectCatalogue);
+menuBtn.addEventListener('click', toggleMenu);
+logoJohn.addEventListener('click',redirectHome);
+
+function redirectHome(){
+    console.log("home")
+    window.location='index.html'
+}
+
+function redirectCatalogue(){
+    console.log("catalogue")
+    window.location='catalogue.html'
+}
+
+function toggleMenu() {
+    console.log("ha");
+    if (!showMenu) {
+        
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        // menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+
+        showMenu = true;
+    } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        // menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+
+        showMenu = false;
+    }
+}
+
